1a52a1cfe4996c0522c74629413259cf2e3a5519
Karankali77 <92243178+Karankali77@users.noreply.github.com>
2021-10-11 14:12:14 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = f "{file_name} \n @C_Y_B_E_R_101,
+                caption = f"{file_name} \n @C_Y_B_E_R_101,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
