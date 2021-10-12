ff74925a0b81afb3182ef6aa129dac7c000e696e
Karankali77 <92243178+Karankali77@users.noreply.github.com>
2021-10-12 23:59:56 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f" Bravo!!🔥 Found 😎 {(len_results)} 🔘💯Results For Your Query🔘: <code>{query}</code>",
+                text=f" Bravo!!🔥 Found  {(len_results)} 💯Results For Your Query🔘: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
