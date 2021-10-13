07be0ccf74e7d2353010a016640b5ef575729abb
Karankali77 <92243178+Karankali77@users.noreply.github.com>
2021-10-13 18:21:39 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -64,11 +64,7 @@ async def start(bot, update):
                     ]
                 )
             )
-                           buttons = [[
-                                   InlineKeyboardButton('Channel', url='https://t.me/aurous')
-                                      ]]
-                                      reply_markup = InlineKeyboardMarkup(buttons)
-
+                           
         elif file_type == "audio":
 
             await bot.send_audio(
