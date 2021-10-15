2cb68c36134b770ff0ed4b62a0b7cacade4b4228
Karankali77 <92243178+Karankali77@users.noreply.github.com>
2021-10-15 10:31:35 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"{file_name}  \n @C_Y_B_E_R_101",
+                caption = f"{file_name}  \n @C_Y_B_E_R_101 \n @movieengine \n @aurousdiscussion ",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
